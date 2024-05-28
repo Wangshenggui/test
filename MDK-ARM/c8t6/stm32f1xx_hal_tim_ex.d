9bed551a56914acc4d972cad385b7f23fe731649
Wangshenggui <1785193871@qq.com>
2024-05-28 16:37:34 +0800

    李四开启USART3

--- MDK-ARM/c8t6/stm32f1xx_hal_tim_ex.d
+++ MDK-ARM/c8t6/stm32f1xx_hal_tim_ex.d
@@ -24,3 +24,4 @@ c8t6\stm32f1xx_hal_tim_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_c
 c8t6\stm32f1xx_hal_tim_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
 c8t6\stm32f1xx_hal_tim_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
 c8t6\stm32f1xx_hal_tim_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+c8t6\stm32f1xx_hal_tim_ex.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
